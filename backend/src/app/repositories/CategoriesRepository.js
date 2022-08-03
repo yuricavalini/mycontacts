467f596b0a16969a3371bcd49ed7b8638a9e9e2d
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-03 17:39:07 -0300

    feat: add basic categories route, controller and repository

---
+++ backend/src/app/repositories/CategoriesRepository.js
@@ -0,0 +1,23 @@
+const db = require('../../database');
+
+class CategoryRepository {
+  async findAll() {
+    const rows = await db.query('SELECT * FROM categories ORDER BY name');
+    return rows;
+  }
+
+  async create({ name }) {
+    const [row] = await db.query(
+      `
+      INSERT INTO categories(name)
+      VALUES($1)
+      RETURNING *
+    `,
+      [name],
+    );
+
+    return row;
+  }
+}
+
+module.exports = new CategoryRepository();
