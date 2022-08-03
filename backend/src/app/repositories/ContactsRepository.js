af3ea34445bbf557a61aa93cde2e31ad89dff3f4
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-03 17:11:09 -0300

    refactor: change ContactController update method to interact with postgresql

--- backend/src/app/repositories/ContactsRepository.js
+++ backend/src/app/repositories/ContactsRepository.js
@@ -56,20 +56,17 @@ class ContactsRepository {
     return row;
   }
 
-  update(id, { name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-
-      contacts = contacts.map((contact) => (contact.id === id ? updatedContact : contact));
-
-      resolve(updatedContact);
-    });
+  async update(id, { name, email, phone, category_id }) {
+    const [row] = await db.query(
+      `
+    UPDATE contacts
+    SET name = $1, email = $2, phone = $3, category_id = $4
+    WHERE id = $5
+    RETURNING *
+    `,
+      [name, email, phone, category_id, id],
+    );
+    return row;
   }
 }
 
