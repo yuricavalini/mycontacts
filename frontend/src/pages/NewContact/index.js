b8bfaa3a361920352179ce3c12baca8ea3620b9b
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-16 19:42:20 -0300

    feat: add Input component

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,5 +1,11 @@
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 
 export default function NewContact() {
-  return <PageHeader title="Novo Contato" />;
+  return (
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input placeholder="nome" />
+    </>
+  );
 }
