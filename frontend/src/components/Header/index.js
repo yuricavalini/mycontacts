c2cc7f7b13a80f349a7dbc5247eab915dec033f4
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-16 19:19:30 -0300

    refactor: change Home page and Header component

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { Container, InputSearchContainer } from './style';
+import { Container } from './style';
 
 import logo from '../../assets/images/logo.svg';
 
@@ -6,10 +6,6 @@ export default function Header() {
   return (
     <Container>
       <img src={logo} alt="MyContacts" width="201" />
-
-      <InputSearchContainer>
-        <input type="text" placeholder="Pesquise pelo nome..." />
-      </InputSearchContainer>
     </Container>
   );
 }
