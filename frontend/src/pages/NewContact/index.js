522e65f470736ae96678518e895cb74113df63cb
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-16 19:44:15 -0300

    feat: add Select component

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,11 +1,18 @@
-import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo Contato" />
       <Input placeholder="nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
     </>
   );
 }
