30a7692f1837490c95cd5a31133ecd038fadac31
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-22 12:47:07 -0300

    feat: add loading status and loading component

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useMemo } from 'react';
-
 import { Link } from 'react-router-dom';
 
 import {
@@ -10,22 +9,36 @@ import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
+import Loader from '../../components/Loader';
+
+import delay from '../../utils/delay';
+
 export default function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
   const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const filteredContacts = useMemo(() => contacts.filter((contact) => (
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
+        await delay(500);
+
         const json = await response.json();
         setContacts(json);
       })
-      .catch((error) => console.log('erro', error));
+      .catch((error) => {
+        console.log('erro', error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [orderBy]);
 
   function handleToggleOrderBy() {
@@ -40,6 +53,8 @@ export default function Home() {
 
   return (
     <Container>
+      <Loader isLoading={isLoading} />
+
       <InputSearchContainer>
         <input
           value={searchTerm}
