5f10d45ebfc8aaf50413314dbaa386b9db22c3c4
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-23 07:45:56 -0300

    feat: add contacts service applying service layer and singleton patterns

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -11,7 +11,7 @@ import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
 
-import delay from '../../utils/delay';
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -28,14 +28,8 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-        const response = await fetch(
-          `http://localhost:3001/contacts?orderBy=${orderBy}`,
-        );
-
-        await delay(500);
-
-        const json = await response.json();
-        setContacts(json);
+        const contactsList = await ContactsService.listContacts(orderBy);
+        setContacts(contactsList);
       } catch (error) {
         console.log('erro', error);
       } finally {
