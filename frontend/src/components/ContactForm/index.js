25b528eae88b08d0fc8e954852f2a4d21d375721
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-24 19:41:32 -0300

    feat: add contacts creation

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -13,7 +13,7 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
-export default function ContactForm({ buttonLabel }) {
+export default function ContactForm({ buttonLabel, onSubmit }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -70,9 +70,9 @@ export default function ContactForm({ buttonLabel }) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
-    console.log({
-      name, email, phone: phone.replace(/\D/g, ''), category: categoryId,
+
+    onSubmit({
+      name, email, phone, categoryId,
     });
   }
 
@@ -136,4 +136,5 @@ export default function ContactForm({ buttonLabel }) {
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,12 +1,33 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContact(contact);
+
+      console.log(response);
+    } catch {
+      alert('ocorreu um erro ao cadastrar o contato!');
+    }
+  }
+
   return (
     <>
       <PageHeader title="Novo Contato" />
 
-      <ContactForm buttonLabel="Cadastrar" />
+      <ContactForm
+        buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
