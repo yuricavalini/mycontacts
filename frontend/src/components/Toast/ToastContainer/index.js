102b463d266053341d2daa635e634eada73d924c
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-27 09:07:15 -0300

    feat: add custom event to the Toast component

--- frontend/src/components/Toast/ToastContainer/index.js
+++ frontend/src/components/Toast/ToastContainer/index.js
@@ -1,12 +1,38 @@
-import ToastMessage from '../ToastMessage';
+import { useState, useEffect } from 'react';
+
 import { Container } from './styles';
 
+import ToastMessage from '../ToastMessage';
+
 export default function ToastContainer() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    function handleAddToast(event) {
+      const { type, text } = event.detail;
+
+      setMessages((prevState) => [
+        ...prevState,
+        { id: Math.random(), type, text },
+      ]);
+    }
+
+    document.addEventListener('addtoast', handleAddToast);
+
+    return () => {
+      document.removeEventListener('addtoast', handleAddToast);
+    };
+  }, []);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
