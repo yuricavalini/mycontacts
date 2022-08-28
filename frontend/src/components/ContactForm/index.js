40ff275a22b01c89ec933c38580bc3d48fc1c740
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-28 13:05:42 -0300

    feat: add updateContact method to ContactsService and implements it in EditContact

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -90,11 +90,6 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     });
 
     setIsSubmitting(false);
-
-    setName('');
-    setEmail('');
-    setPhone('');
-    setCategoryId('');
   }
 
   return (
