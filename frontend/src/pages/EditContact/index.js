e3b3e80fea28bd0a58e0031568dbb0825e843b1a
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-28 11:15:41 -0300

    feat: load contact data in EditContact page

--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,12 +1,55 @@
+import { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import ContactsService from '../../services/ContactsService';
+
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import Loader from '../../components/Loader';
+
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  console.log(id);
+  console.log(history);
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader title="Editar Yuri" />
 
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
