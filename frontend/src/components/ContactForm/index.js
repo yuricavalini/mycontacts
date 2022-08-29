bc0eaebd6b31072d8aba34c5714daaec267a2596
Yuri Cavalini <yuricavalini@gmail.com>
2022-08-29 14:39:18 -0300

    feat: add useSafeAsyncState custom hook

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -14,14 +14,15 @@ import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
+import useSafeAsyncState from '../../hooks/useSafeAsyncState';
 
 const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [categoryId, setCategoryId] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [categories, setCategories] = useSafeAsyncState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const {
@@ -60,7 +61,7 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     }
 
     loadCategories();
-  }, []);
+  }, [setCategories, setIsLoadingCategories]);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
---
+++ frontend/src/hooks/useSafeAsyncState.js
@@ -0,0 +1,25 @@
+import {
+  useState, useRef, useEffect, useCallback,
+} from 'react';
+
+export default function useSafeAsyncState(initialState) {
+  const [state, setState] = useState(initialState);
+
+  const isMounted = useRef();
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const setSafeAsyncState = useCallback((data) => {
+    if (isMounted.current) {
+      setState(data);
+    }
+  }, []);
+
+  return [state, setSafeAsyncState];
+}
